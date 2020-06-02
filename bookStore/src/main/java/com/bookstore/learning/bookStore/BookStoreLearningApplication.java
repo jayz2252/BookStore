0fb80acc71fa7b3cfc36871d5e0a6371aa04e568
Jayanth <jayanthmakam48@gmail.com>
2020-06-02 22:28:12 +0530

    Initial configuration(starter project)

---
+++ bookStore/src/main/java/com/bookstore/learning/bookStore/BookStoreLearningApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore.learning.bookStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreLearningApplication.class, args);
+	}
+
+}
